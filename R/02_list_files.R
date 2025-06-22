ce9c8ba0d2366d1fdf3fa4d2eb0af86b2b3f3ca5
muschellij2 <muschellij2@gmail.com>
2025-06-22 15:45:42 -0400

    added listing files

--- R/02_list_files.R
+++ R/02_list_files.R
@@ -14,48 +14,35 @@ df = tibble(
 )
 df = df %>% 
   mutate(
-    file_nifti_ct = sub("nifti/", "label/", file_nifti_ct),
-    file_nifti_roi = ifelse(!file.exists(file_nifti_roi), NA, file_nifti_roi)
+    file_nifti_roi = sub("nifti/", "label/", file_nifti_ct),
+    file_nifti_roi = ifelse(!file.exists(file_nifti_roi), NA, file_nifti_roi),
+    group = ifelse(!file.exists(file_nifti_roi), "evaluation", "train"),
+    id = nii.stub(file_nifti_ct, bn = TRUE),
+    fold = as.numeric(factor(id))
   )
-# d
 
-df_dir = expand_grid(
-  # stage_number = 1:2,
-  stage_number = 2,
-  group = c("train", "test")
-) %>% 
-  mutate(
-    dir = here::here("data", paste0("nifti", stage_number, "_", group, "_images"))
-  ) %>% 
-  filter(dir.exists(dir))
-print(df_dir)
+dir_ss = here::here("data", "brain_extracted")
+dir_mask = here::here("data", "brain_mask")
 
-paths = df_dir$dir
-df = map_df(paths, function(path) {
-  files = list.files(path, full.names = TRUE, recursive = TRUE, pattern = "\\.dcm$")
-  tibble(
-    file = files,
-    fname = basename(files),
-    dir = path
+fs::dir_create(
+  c(
+    dir_ss,
+    dir_mask
   )
-}, .progress = TRUE)
-
-df = df %>% 
-  dplyr::left_join(df_dir)
+)
 
 df = df %>%
-  mutate(id = sub("\\.dcm$", "", fname)) 
-stopifnot(anyDuplicated(df$id) == 0)
-
-n_folds = 200
-df = df %>% 
   mutate(
-    hdr = here::here("data", "header", paste0(id, ".rds")),
-    fold = (0:(nrow(df)-1)) %% n_folds + 1
-  )
+    stub = basename(file_nifti_ct),
 
-fs::dir_create(here::here("data", "header"))
-outfile = here::here("data", "dicom_filenames.rds")
+    file_ss = here::here(dir_ss, stub),
+    file_mask = here::here(dir_mask, stub),
+    
+  ) %>%
+  select(-stub)
+
+
+outfile = here::here("data", "filenames.rds")
 readr::write_rds(df, outfile)
 
 
