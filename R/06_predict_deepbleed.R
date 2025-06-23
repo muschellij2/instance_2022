9e95b651ab1ea6bc4690e4d3620efeae1f8881a7
muschellij2 <muschellij2@gmail.com>
2025-06-23 11:32:19 -0400

    added in rpi

--- R/06_predict_deepbleed.R
+++ R/06_predict_deepbleed.R
@@ -6,6 +6,7 @@ library(here)
 library(purrr)
 library(tidyr)
 library(ichseg)
+library(fslr)
 source(here::here("R/utils.R"))
 
 file = here::here("data", "filenames.rds")
@@ -16,9 +17,16 @@ if (!file.exists(file.path(dir_model, "checkpoint"))) {
   ichseg::download_deepbleed_model(outdir = dir_model)
 }
 
-iid = 1
+pkgs = c("numpy<2", "nibabel", "pillow", "six", "scikit-learn", "scikit-image", 
+  "webcolors", "plotly", "pandas", "matplotlib", "h5py>=2.9", "fslpy", 
+  "gast==0.2.2", "tensorflow<2.16", "statsmodels")
+reticulate::py_require(pkgs)
+iid = which(df$id == "077")
 idf = df[iid,]
 
-pred = ichseg::predict_deepbleed(image = idf$file_nifti_ct,
-                          mask = idf$file_mask,
-                          outdir = dir_model)
+res = rpi_orient_file(file = idf$file_nifti_ct)
+res_mask = rpi_orient_file(file = idf$file_mask)
+pred = ichseg::predict_deepbleed(
+  image = res$img,
+  mask = res_mask$img,
+  outdir = dir_model)
