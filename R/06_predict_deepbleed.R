4e6c6aea7c348ffbbb3622f8affe9fd5610ad57b
muschellij2 <muschellij2@gmail.com>
2025-06-23 12:32:34 -0400

    predict deepbleed ready

--- R/06_predict_deepbleed.R
+++ R/06_predict_deepbleed.R
@@ -18,15 +18,29 @@ if (!file.exists(file.path(dir_model, "checkpoint"))) {
 }
 
 pkgs = c("numpy<2", "nibabel", "pillow", "six", "scikit-learn", "scikit-image", 
-  "webcolors", "plotly", "pandas", "matplotlib", "h5py>=2.9", "fslpy", 
-  "gast==0.2.2", "tensorflow<2.16", "statsmodels")
+         "webcolors", "plotly", "pandas", "matplotlib", "h5py>=2.9", "fslpy", 
+         "gast==0.2.2", "tensorflow<2.16", "statsmodels")
 reticulate::py_require(pkgs)
 iid = which(df$id == "077")
-idf = df[iid,]
 
-res = rpi_orient_file(file = idf$file_nifti_ct)
-res_mask = rpi_orient_file(file = idf$file_mask)
-pred = ichseg::predict_deepbleed(
-  image = res$img,
-  mask = res_mask$img,
-  outdir = dir_model)
+
+for (iid in seq(nrow(df))) {
+  idf = df[iid,]
+  
+  if (!file.exists(idf$file_nifti_prediction)) {
+    res = rpi_orient_file(file = idf$file_nifti_ct)
+    res_mask = rpi_orient_file(file = idf$file_mask)
+    if (file.exists(idf$file_nifti_roi)) {
+      res_roi = rpi_orient_file(idf$file_nifti_roi)
+    }
+    pred = ichseg::predict_deepbleed(
+      image = res$img,
+      mask = res_mask$img,
+      outdir = dir_model)
+    
+    native = pred$native_prediction
+    native[abs(native) < .Machine$double.eps] = 0
+    native = reverse_rpi_orient(native, convention = res$convention, orientation = res$orientation)
+    neurobase::writenii(native, idf$file_nifti_prediction)
+  }
+}
