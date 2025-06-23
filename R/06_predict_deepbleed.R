f85ed3e410d20ddaebf23bd100ae85f7b3ef8623
muschellij2 <muschellij2@gmail.com>
2025-06-23 11:08:33 -0400

    fixing predict deepbleed

--- R/06_predict_deepbleed.R
+++ R/06_predict_deepbleed.R
@@ -1,3 +1,4 @@
+Sys.setenv("RETICULATE_PYTHON" = "managed")
 library(neurobase)
 library(dplyr)
 library(readr)
@@ -10,4 +11,14 @@ source(here::here("R/utils.R"))
 file = here::here("data", "filenames.rds")
 df = readRDS(file)
 
+dir_model = here::here("deepbleed_model")
+if (!file.exists(file.path(dir_model, "checkpoint"))) {
+  ichseg::download_deepbleed_model(outdir = dir_model)
+}
 
+iid = 1
+idf = df[iid,]
+
+pred = ichseg::predict_deepbleed(image = idf$file_nifti_ct,
+                          mask = idf$file_mask,
+                          outdir = dir_model)
